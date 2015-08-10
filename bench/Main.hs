d3d6ae154a00664dc332c0c5df8f280bd4e0186a
Adam C. Foltzer <acfoltzer@galois.com>
2015-08-10 13:52:01 -0700

    add typechecking and large sequence benchmarks

--- bench/Main.hs
+++ bench/Main.hs
@@ -7,11 +7,20 @@
 -- Portability :  portable
 module Main where
 
+import Control.DeepSeq
+
 import qualified Data.Text.Lazy     as T
 import qualified Data.Text.Lazy.IO  as T
 
-import qualified Cryptol.Parser     as P
-import qualified Cryptol.Parser.AST as P
+import qualified Cryptol.ModuleSystem.Base  as M
+import qualified Cryptol.ModuleSystem.Env   as M
+import qualified Cryptol.ModuleSystem.Monad as M
+
+import qualified Cryptol.Parser           as P
+import qualified Cryptol.Parser.AST       as P
+import qualified Cryptol.Parser.NoInclude as P
+
+import qualified Cryptol.TypeCheck as T
 
 import Criterion.Main
 
@@ -19,6 +28,11 @@ main :: IO ()
 main = defaultMain [
     bgroup "parser" [
         parser "Prelude" "lib/Cryptol.cry"
+      , parser "BigSequence" "bench/data/BigSequence.cry"
+      ]
+  , bgroup "typechecker" [
+        tc "Prelude" "lib/Cryptol.cry"
+      , tc "BigSequence" "bench/data/BigSequence.cry"
       ]
   ]
 
@@ -34,3 +48,38 @@ parser name path =
       case P.parseModule cfg bytes of
         Right pm -> return pm
         Left err -> error (show err)
+
+-- | Make a benchmark for typechecking a Cryptol module. Does parsing
+-- in the setup phase in order to isolate typechecking
+tc :: String -> FilePath -> Benchmark
+tc name path =
+  let setup = do
+        bytes <- T.readFile path
+        let cfg = P.defaultConfig
+                { P.cfgSource  = path
+                , P.cfgPreProc = P.guessPreProc path
+                }
+            Right pm = P.parseModule cfg bytes
+        menv <- M.initialModuleEnv
+        (Right (scm, menv'), _) <- M.runModuleM menv $ do
+          -- code from `loadModule` and `checkModule` in
+          -- `Cryptol.ModuleSystem.Base`
+          let pm' = M.addPrelude pm
+          M.loadDeps pm'
+          Right nim <- M.io (P.removeIncludesModule path pm')
+          npm <- M.noPat nim
+          M.renameModule npm
+        return (scm, menv')
+  in env setup $ \ ~(scm, menv) ->
+    bench name $ whnfIO $ M.runModuleM menv $ do
+      let act = M.TCAction { M.tcAction = T.tcModule
+                           , M.tcLinter = M.moduleLinter (P.thing (P.mName scm)) }
+      M.typecheck act scm =<< M.importIfacesTc (map P.thing (P.mImports scm))
+
+-- FIXME: this should only throw off the first benchmark run, but still...
+instance NFData P.Module where
+  rnf _ = ()
+
+-- FIXME: this should only throw off the first benchmark run, but still...
+instance NFData M.ModuleEnv where
+  rnf _ = ()
