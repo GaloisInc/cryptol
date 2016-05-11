70459264ab7c799d853ccb5edda7114ffd06f527
Brian Huffman <huffman@galois.com>
2016-05-11 09:26:57 -0700

    Use hang and sep instead of hsep to print counterexamples.
    Fixes #329.

--- src/Cryptol/REPL/Command.hs
+++ src/Cryptol/REPL/Command.hs
@@ -439,7 +439,7 @@ cmdProveSat isSat expr = do
                 let docs = map (pp . E.WithBase ppOpts) vs
                     -- function application has precedence 3
                     doc = ppPrec 3 parseExpr
-                rPrint $ hsep (doc : docs) <+>
+                rPrint $ hang doc 2 (sep docs) <+>
                   text (if isSat then "= True" else "= False")
           resultRecs <- mapM (mkSolverResult cexStr isSat . Right) tess
           let collectTes tes = (t, es)
