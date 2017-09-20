c4af07a05363e9d814ae78cfd30fb26ce4659545
Brian Huffman <huffman@galois.com>
2017-09-20 11:38:36 -0700

    Remove unused TokenKW constructors.

--- src/Cryptol/Parser/LexerUtils.hs
+++ src/Cryptol/Parser/LexerUtils.hs
@@ -358,12 +358,7 @@ data TokenV   = VCurlyL| VCurlyR | VSemi
 data TokenW   = BlockComment | LineComment | Space | DocStr
                 deriving (Eq, Show, Generic, NFData)
 
-data TokenKW  = KW_Arith
-              | KW_Bit
-              | KW_Cmp
-              | KW_SignedCmp
-              | KW_else
-              | KW_Eq
+data TokenKW  = KW_else
               | KW_extern
               | KW_fin
               | KW_if
