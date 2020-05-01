b196855c27c1537a9a535d3cb044e41476701b06
Jared Weakly <jweakly@galois.com>
2020-05-01 09:54:30 -0700

    make script stricter

--- .github/ci.sh
+++ .github/ci.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
+set -Eeuo pipefail
 
+[[ "$RUNNER_OS" == 'Windows' ]] && IS_WIN=true || IS_WIN=false
 BIN=bin
 EXT=""
-[[ "$RUNNER_OS" == 'Windows' ]] && EXT=".exe"
+$IS_WIN && EXT=".exe"
 mkdir -p "$BIN"
 
-function is_exe() { [[ -x "$1/$2$EXT" ]] || which "$2" > /dev/null 2>&1; }
+is_exe() { [[ -x "$1/$2$EXT" ]] || command -v "$2" > /dev/null 2>&1; }
 
-function extract_exe() {
+extract_exe() {
   exe="$(cabal v2-exec which "$1$EXT")"
   name="$(basename "$exe")"
   echo "Copying $name to $2"
@@ -15,19 +17,19 @@ function extract_exe() {
   cp -f "$exe" "$2/$name"
 }
 
-function setup_external_tools() {
+setup_external_tools() {
   is_exe "$BIN" "test-runner" && return
   cabal v2-install --install-method=copy --installdir="$BIN" test-lib
 }
 
-function setup_dist_bins() {
+setup_dist_bins() {
   is_exe "dist" "cryptol" && is_exe "dist" "cryptol-html" && return
   extract_exe "cryptol" "dist"
   extract_exe "cryptol-html" "dist"
   strip dist/cryptol*
 }
 
-function install_z3() {
+install_z3() {
   is_exe "$BIN" "z3" && return
 
   case "$RUNNER_OS" in
@@ -37,13 +39,13 @@ function install_z3() {
   esac
   curl -o z3.zip -sL "https://github.com/Z3Prover/z3/releases/download/z3-$Z3_VERSION/z3-$Z3_VERSION-x64-$file"
 
-  if [[ "$RUNNER_OS" == 'Windows' ]]; then 7z x -bd z3.zip else unzip z3.zip; fi
+  if $IS_WIN; then 7z x -bd z3.zip; else unzip z3.zip; fi
   cp z3-*/bin/z3$EXT $BIN/z3$EXT
-  [[ "$RUNNER_OS" != 'Windows' ]] && chmod +x $BIN/z3
+  $IS_WIN || chmod +x $BIN/z3
   rm z3.zip
 }
 
-function install_cvc4() {
+install_cvc4() {
   is_exe "$BIN" "cvc4" && return
   version="${CVC4_VERSION#4.}" # 4.y.z -> y.z
 
@@ -52,12 +54,12 @@ function install_cvc4() {
     Windows) file="win64-opt.exe" ;;
     macOS) brew tap cvc4/cvc4 && brew install cvc4/cvc4/cvc4 && return ;;
   esac
-  curl -o cvc4 -sL "https://github.com/CVC4/CVC4/releases/download/1.7/cvc4-$version-$file"
-  [[ "$RUNNER_OS" != 'Windows' ]] && chmod +x cvc4
-  mv cvc4 "$BIN/cvc4$EXT"
+  curl -o cvc4$EXT -sL "https://github.com/CVC4/CVC4/releases/download/1.7/cvc4-$version-$file"
+  $IS_WIN || chmod +x cvc4$EXT
+  mv cvc4$EXT "$BIN/cvc4$EXT"
 }
 
-function install_yices() {
+install_yices() {
   is_exe "$BIN" "yices" && return
   ext=".tar.gz"
   case "$RUNNER_OS" in
@@ -89,10 +91,13 @@ install_deps() {
 }
 
 install_system_deps() {
-  install_z3 "$Z3_VERSION"
-  install_cvc4 "$CVC4_VERSION"
-  install_yices "$YICES_VERSION"
-  echo "::add-path::$PWD/bin"
+  install_z3 &
+  install_cvc4 &
+  install_yices &
+  wait
+  export PATH=$PWD/$BIN:$PATH
+  echo "::add-path::$PWD/$BIN"
+  is_exe "$BIN" z3 && is_exe "$BIN" cvc4 && is_exe "$BIN" yices
 }
 
 test_dist() {
