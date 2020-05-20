a812058dcc503d63ad2222ce1eabe41638575926
Iavor Diatchki <iavor.diatchki@gmail.com>
2020-05-20 16:41:43 -0700

    Make function to parse bits into a float into a pure function.
    This is useful for counter example in What4

--- src/Cryptol/Eval/Concrete/Float.hs
+++ src/Cryptol/Eval/Concrete/Float.hs
@@ -1,5 +1,4 @@
 {-# Language BlockArguments #-}
-{-# Language MultiWayIf #-}
 {-# Language OverloadedStrings #-}
 -- | Concrete evaluations for floating point primitives.
 module Cryptol.Eval.Concrete.Float where
@@ -11,6 +10,7 @@ import qualified Data.Map as Map
 import LibBF
 
 import Cryptol.Utils.Ident(Ident, floatPrimIdent)
+import Cryptol.Utils.Panic(panic)
 import Cryptol.Eval.Value
 import Cryptol.Eval.Concrete.FloatHelpers
 import Cryptol.Eval.Concrete.Value
@@ -23,7 +23,9 @@ floatPrims sym = Map.fromList [ (floatPrimIdent i,v) | (i,v) <- table ]
     [ "fpNaN"     ~> ilam \_ -> ilam \_ -> VFloat bfNaN
     , "fpPosInf"  ~> ilam \_ -> ilam \_ -> VFloat bfPosInf
     , "fpNeg"     ~> ilam \_ -> ilam \_ -> flam \x -> pure $ VFloat $ bfNeg x
-    , "fpFromBits" ~> fpFromBits sym
+    , "fpFromBits" ~> ilam \e -> ilam \p -> wlam sym \bv ->
+                        pure $ VFloat $ floatFromBits e p $ bvVal bv
+
 
     , "fpAdd"     ~> binArith sym bfAdd
     , "fpSub"     ~> binArith sym bfSub
@@ -44,49 +46,49 @@ binArith sym fun =
   do opts <- floatOpts sym e p (bvVal r)
      VFloat <$> checkStatus sym (fun opts x y)
 
-fpFromBits :: Concrete -> Value
-fpFromBits sym =
-  ilam \e ->
-  ilam \p ->
-  wlam sym \bv ->
-  do opts <- floatOpts sym e p 0
-     let bits       = bvVal bv
-         e'         = fromInteger e                               :: Int
-         p'         = fromInteger p - 1                           :: Int
-         eMask      = (1 `shiftL` e') - 1                         :: Int64
-         pMask      = (1 `shiftL` p') - 1                         :: Integer
 
-         isNeg      = testBit bits (e' + p')
+-- | Make a float using "raw" bits.
+floatFromBits ::
+  Integer {- ^ Exponent width -} ->
+  Integer {- ^ Precision widht -} ->
+  Integer {- ^ Raw bits -} ->
+  BigFloat
 
-         mant       = pMask .&. bits                              :: Integer
-         mantVal    = if expoBiased == 0 
-                        then mant
-                        else mant `setBit` p'                     :: Integer
-         -- accounts for the implicit 1 bit
+floatFromBits e p bits
 
-         expoBiased = eMask .&. fromInteger (bits `shiftR` p')    :: Int64
-         bias       = eMask `shiftR` 1                            :: Int64
-         expoVal    = expoBiased - bias - fromIntegral p'         :: Int64
+  | expoBiased == 0 && mant == 0 =            -- zero
+    if isNeg then bfNegZero else bfPosZero
+
+  | expoBiased == eMask && mant ==  0 =       -- infinity
+    if isNeg then bfNegInf else bfPosInf
+
+  | expoBiased == eMask = bfNaN               -- NaN
+
+  | otherwise =                               -- A "normal" float
+     case bfMul2Exp infPrec (bfFromInteger mantVal) expoVal of
+       (num,Ok) -> if isNeg then bfNeg num else num
+       (_,s) -> panic "floatFromBits" [ "Unexpected status: " ++ show s ]
+
+  where
+  e'         = fromInteger e                               :: Int
+  p'         = fromInteger p - 1                           :: Int
+  eMask      = (1 `shiftL` e') - 1                         :: Int64
+  pMask      = (1 `shiftL` p') - 1                         :: Integer
+
+  isNeg      = testBit bits (e' + p')
+
+  mant       = pMask .&. bits                              :: Integer
+  mantVal    = if expoBiased == 0 
+                 then mant
+                 else mant `setBit` p'                     :: Integer
+  -- accounts for the implicit 1 bit
+
+  expoBiased = eMask .&. fromInteger (bits `shiftR` p')    :: Int64
+  bias       = eMask `shiftR` 1                            :: Int64
+  expoVal    = expoBiased - bias - fromIntegral p'         :: Int64
 
 
-     VFloat <$>
-       if -- zero
-          | expoBiased == 0 && mant == 0 ->
-            pure $ if isNeg then bfNegZero else bfPosZero
 
-          -- infinity
-          | expoBiased == eMask && mant ==  0 ->
-            pure $ if isNeg then bfNegInf else bfPosInf
 
-          -- NaN
-          | expoBiased == eMask ->
-            pure $ bfNaN
-
-          -- A "normal" float
-          | otherwise ->
-            do num <- checkStatus sym
-                        (bfMul2Exp opts (bfFromInteger mantVal) expoVal)
-
-               pure (if isNeg then bfNeg num else num)
 
 
