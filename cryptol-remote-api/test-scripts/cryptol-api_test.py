b8d32d63aa60bde33a08037cebdca4e36b179b19
Andrew Kent <andrew@galois.com>
2020-11-10 11:31:37 -0800

    cryptol-remote-api feature: use python BV class cryptol bits

--- cryptol-remote-api/test-scripts/cryptol-api_test.py
+++ cryptol-remote-api/test-scripts/cryptol-api_test.py
@@ -1,8 +1,9 @@
 import os
-
+import unittest
 from cryptol import CryptolConnection, CryptolContext, cry
 import cryptol
 import cryptol.cryptoltypes
+from cryptol.bitvector import BV
 from BitVector import *
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -13,19 +14,28 @@ c.change_directory(dir_path)
 
 c.load_file("Foo.cry")
 
-x_val = c.evaluate_expression("x").result()
+class CryptolTests(unittest.TestCase):
 
-assert c.evaluate_expression("Id::id x").result() == x_val
-assert c.call('Id::id', bytes.fromhex('ff')).result() == bytes.fromhex('ff')
+    def test_low_level(self):
+        x_val = c.evaluate_expression("x").result()
+
+        self.assertEqual(c.evaluate_expression("Id::id x").result(), x_val)
+        self.assertEqual(c.call('Id::id', bytes.fromhex('ff')).result(), BV(8,255))
+
+        self.assertEqual(c.call('add', b'\0', b'\1').result(), BV(8,1))
+        self.assertEqual(c.call('add', bytes.fromhex('ff'), bytes.fromhex('03')).result(), BV(8,2))
+
+    def test_module_import(self):
+        cryptol.add_cryptol_module('Foo', c)
+        from Foo import add
+        self.assertEqual(add(b'\2', 2), BV(8,4))
+
+        self.assertEqual(add(BitVector( intVal = 0, size = 8 ), BitVector( intVal = 1, size = 8 )), BV(8,1))
+        self.assertEqual(add(BitVector( intVal = 1, size = 8 ), BitVector( intVal = 2, size = 8 )), BV(8,3))
+        self.assertEqual(add(BitVector( intVal = 255, size = 8 ), BitVector( intVal = 1, size = 8 )), BV(8,0))
+        self.assertEqual(add(BV(8,0),   BV(8,1)), BV(8,1))
+        self.assertEqual(add(BV(8,1),   BV(8,2)), BV(8,3))
+        self.assertEqual(add(BV(8,255), BV(8,1)), BV(8,0))
 
 
-assert c.call('add', b'\0', b'\1').result() == b'\x01'
-assert c.call('add', bytes.fromhex('ff'), bytes.fromhex('03')).result() == bytes.fromhex('02')
-
-cryptol.add_cryptol_module('Foo', c)
-from Foo import *
-assert add(b'\2', 2) == b'\4'
-
-assert add(BitVector( intVal = 0, size = 8 ), BitVector( intVal = 1, size = 8 )) == bytes.fromhex('01')
-assert add(BitVector( intVal = 1, size = 8 ), BitVector( intVal = 2, size = 8 )) == bytes.fromhex('03')
-assert add(BitVector( intVal = 255, size = 8 ), BitVector( intVal = 1, size = 8 )) == bytes.fromhex('00')
+unittest.main()
