ddeb28198e1447589f1f0525dc75d0b7b9da8b95
Andrew Kent <andrew@galois.com>
2021-03-18 15:36:00 -0700

    chore: squelch some warnings in CryptolServer

--- cryptol-remote-api/src/CryptolServer/Data/Type.hs
+++ cryptol-remote-api/src/CryptolServer/Data/Type.hs
@@ -211,7 +211,7 @@ instance JSON.ToJSON JSONType where
         , "name" .= show (ppWithNames ns v)
         ]
       convert (TUser _n _args def) = convert def
-      convert (TNewtype nt ts) =
+      convert (TNewtype _nt _ts) =
         error "JSON conversion of newtypes is not yet supported TODO"
       convert (TRec fields) =
         JSON.object
