600c77a3dafbbfa7da244caed33643be375d8907
Andrew Kent <andrew@galois.com>
2021-09-21 12:53:56 -0700

    fix new tests

--- cryptol-remote-api/python/tests/cryptol/test_basics.py
+++ cryptol-remote-api/python/tests/cryptol/test_basics.py
@@ -48,35 +48,35 @@ class BasicServerTests(unittest.TestCase):
 
     def test_check_timeout(self):
         c = self.c
-        c.load_file(str(Path('tests','cryptol','test-files', 'examples','AES.cry'))).result()
+        c.load_file(str(Path('tests','cryptol','test-files', 'examples','AES.cry')))
 
         t1 = time.time()
         with self.assertRaises(ArgoException):
-            c.check("\\(bv : [256]) -> ~ (~ (~ (~bv))) == bv", num_tests="all", timeout=1.0).result()
+            c.check("\\(bv : [256]) -> ~ (~ (~ (~bv))) == bv", num_tests="all", timeout=1.0)
         t2 = time.time()
         self.assertLess(t2 - t1, 2.0)
 
         t1 = time.time()
         with self.assertRaises(ArgoException):
-            c.check("\\(bv : [256]) -> ~ (~ (~ (~bv))) == bv", num_tests="all", timeout=5.0).result()
+            c.check("\\(bv : [256]) -> ~ (~ (~ (~bv))) == bv", num_tests="all", timeout=5.0)
         t2 = time.time()
         self.assertLess(t2 - t1, 7)
 
         t1 = time.time()
-        c.check("\\(bv : [256]) -> ~ (~ (~ (~bv))) == bv", num_tests=10, timeout=5.0).result()
+        c.check("\\(bv : [256]) -> ~ (~ (~ (~bv))) == bv", num_tests=10, timeout=5.0)
         t2 = time.time()
         self.assertLess(t2 - t1, 5)
 
     def test_interrupt(self):
         c = self.c
-        c.load_file(str(Path('tests','cryptol','test-files', 'examples','AES.cry'))).result()
+        c.load_file(str(Path('tests','cryptol','test-files', 'examples','AES.cry')))
 
         c.check("\\(bv : [256]) -> ~ (~ (~ (~bv))) == bv", num_tests="all")
-        # ^ .result() intentionally omitted so we don't wait on it's result and we can interrupt
+        # ^  intentionally omitted so we don't wait on it's result and we can interrupt
         # it on the next line.
         time.sleep(.5)
         c.interrupt()
-        self.assertTrue(c.safe("aesEncrypt").result())
+        self.assertTrue(c.safe("aesEncrypt"))
 
     def test_prove_timeout(self):
         c = self.c
@@ -84,41 +84,41 @@ class BasicServerTests(unittest.TestCase):
 
         pt = BV(size=128, value=0x3243f6a8885a308d313198a2e0370734)
         key = BV(size=128, value=0x2b7e151628aed2a6abf7158809cf4f3c)
-        ct = c.call("aesEncrypt", (pt, key)).result()
+        ct = c.call("aesEncrypt", (pt, key))
         expected_ct = BV(size=128, value=0x3925841d02dc09fbdc118597196a0b32)
         self.assertEqual(ct, expected_ct)
 
-        decrypted_ct = c.call("aesDecrypt", (ct, key)).result()
+        decrypted_ct = c.call("aesDecrypt", (ct, key))
         self.assertEqual(pt, decrypted_ct)
 
         pt = BV(size=128, value=0x00112233445566778899aabbccddeeff)
         key = BV(size=128, value=0x000102030405060708090a0b0c0d0e0f)
-        ct = c.call("aesEncrypt", (pt, key)).result()
+        ct = c.call("aesEncrypt", (pt, key))
         expected_ct = BV(size=128, value=0x69c4e0d86a7b0430d8cdb78070b4c55a)
         self.assertEqual(ct, expected_ct)
 
-        decrypted_ct = c.call("aesDecrypt", (ct, key)).result()
+        decrypted_ct = c.call("aesDecrypt", (ct, key))
         self.assertEqual(pt, decrypted_ct)
 
-        self.assertTrue(c.safe("aesEncrypt").result())
-        self.assertTrue(c.safe("aesDecrypt").result())
-        self.assertTrue(c.check("AESCorrect").result().success)
+        self.assertTrue(c.safe("aesEncrypt"))
+        self.assertTrue(c.safe("aesDecrypt"))
+        self.assertTrue(c.check("AESCorrect").success)
         t1 = time.time()
         with self.assertRaises(ArgoException):
-            c.prove("AESCorrect", timeout=1.0).result()
+            c.prove("AESCorrect", timeout=1.0)
         t2 = time.time()
         # check the timeout worked
         self.assertGreaterEqual(t2 - t1, 1.0)
         self.assertLess(t2 - t1, 5.0)
 
         # make sure things are still working
-        self.assertTrue(c.safe("aesEncrypt").result())
+        self.assertTrue(c.safe("aesEncrypt"))
 
         # set the timeout at the connection level
         c.timeout = 1.0
         t1 = time.time()
         with self.assertRaises(ArgoException):
-            c.prove("AESCorrect").result()
+            c.prove("AESCorrect")
         t2 = time.time()
         # check the timeout worked
         self.assertGreaterEqual(t2 - t1, 1.0)
@@ -126,20 +126,20 @@ class BasicServerTests(unittest.TestCase):
 
         # make sure things are still working
         c.timeout = None
-        self.assertTrue(c.safe("aesEncrypt").result())
+        self.assertTrue(c.safe("aesEncrypt"))
 
         c.timeout = 1.0
         t1 = time.time()
         with self.assertRaises(ArgoException):
             # override timeout with longer time
-            c.prove("AESCorrect", timeout=5.0).result()
+            c.prove("AESCorrect", timeout=5.0)
         t2 = time.time()
         self.assertGreaterEqual(t2 - t1, 5.0)
         self.assertLess(t2 - t1, 10.0)
 
         # make sure things are still working
         c.timeout = None
-        self.assertTrue(c.safe("aesEncrypt").result())
+        self.assertTrue(c.safe("aesEncrypt"))
 
 
 class BasicLoggingServerTests(unittest.TestCase):
