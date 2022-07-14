936d97aae795d66cbb0bfe48aeca321d849d93f3
Henry Blanchette <henry@monkeyface.com>
2022-07-14 12:56:05 -0700

    annoated where numeric sampler will be invoked

--- src/Cryptol/TypeCheck/Default.hs
+++ src/Cryptol/TypeCheck/Default.hs
@@ -207,19 +207,24 @@ the given type.  This is useful when we do evaluation at the REPL.
 The resulting types should satisfy the constraints of the schema.
 The parameters should be all of numeric kind, and the props should als
 be numeric -}
+-- TODO: I think this also needs to return a generator for solutions, since we
+-- are going to sample multiple times, not just once like was previously
+-- implemented
 defaultReplExpr' :: Solver -> [TParam] -> [Prop] -> IO (Maybe [ (TParam,Type) ])
 defaultReplExpr' sol as props =
   do let params = map tpVar as
      mb <- tryGetModel sol params props
      case mb of
        Nothing -> return Nothing
-       Just mdl0 ->
-         do mdl <- shrinkModel sol params props mdl0
-            let su = listSubst [ (x, tNat' n) | (x,n) <- mdl ]
-            return $
-              do guard (null (concatMap pSplitAnd (apSubst su props)))
-                 tys <- mapM (bindParam su) params
-                 return (zip as tys)
+       Just mdl0 -> do
+        -- TODO: here is where the literal sampler will be invoked
+        -- uses the smt solver first to check that there is a solution
+        mdl <- shrinkModel sol params props mdl0
+        let su = listSubst [ (x, tNat' n) | (x,n) <- mdl ]
+        return $ do
+          guard (null (concatMap pSplitAnd (apSubst su props)))
+          tys <- mapM (bindParam su) params
+          return (zip as tys)
   where
   bindParam su tp =
     do let ty  = TVar tp
