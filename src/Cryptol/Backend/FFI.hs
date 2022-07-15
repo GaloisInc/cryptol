d161e958295f17286865069bbfeb9eca60f393b3
Bretton <qsctr@users.noreply.github.com>
2022-07-15 14:51:09 -0700

    FFI: Refactor loading/unloading shared libraries
    Get rid of manual reference counting and fix leak where dlclose is not
    called if no symbols were successfully loaded

--- src/Cryptol/Backend/FFI.hs
+++ src/Cryptol/Backend/FFI.hs
@@ -1,16 +1,9 @@
 {-# LANGUAGE BlockArguments            #-}
 {-# LANGUAGE CPP                       #-}
-{-# LANGUAGE DeriveAnyClass            #-}
-{-# LANGUAGE DeriveGeneric             #-}
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE RecordWildCards           #-}
 {-# LANGUAGE ScopedTypeVariables       #-}
-{-# LANGUAGE StandaloneDeriving        #-}
 {-# LANGUAGE TypeApplications          #-}
-{-# LANGUAGE TypeSynonymInstances      #-}
-
--- We need some instances that the unix package doesn't define
-{-# OPTIONS_GHC -Wno-orphans #-}
 
 module Cryptol.Backend.FFI
 #ifdef FFI_ENABLED
@@ -27,43 +20,29 @@ module Cryptol.Backend.FFI
 
 #ifdef FFI_ENABLED
 
-import           Control.DeepSeq
 import           Control.Exception
 import           Control.Monad
 import           Data.Bifunctor
-import           Data.IORef
 import           Data.Word
 import           Foreign.Concurrent
 import           Foreign.ForeignPtr         (ForeignPtr, withForeignPtr)
 import           Foreign.LibFFI
 import           Foreign.Ptr
-import           GHC.Generics
 import           System.FilePath            ((-<.>))
 import           System.IO.Error
 import           System.Posix.DynamicLinker
 
 import           Cryptol.Backend.FFI.Error
 
--- | A 'ForeignSrc' consists of a handle to the dynamically loaded library and
--- a reference count for the number of foreign functions from the library that
--- are still in memory.
--- When all references to the foreign functions are garbage collected the
--- library will be closed by the 'ForeignPtr' finalizer in 'loadForeignImpl'.
-data ForeignSrc = ForeignSrc
-  { foreignLib  :: !ForeignLib
-  , foreignRefs :: !(IORef Word) }
-
-type ForeignLib = DL
-
-deriving instance Generic ForeignLib
-deriving instance NFData ForeignLib
+newtype ForeignSrc = ForeignSrc (ForeignPtr ())
 
 loadForeignSrc :: FilePath -> IO (Either FFILoadError ForeignSrc)
-loadForeignSrc = loadForeignLib >=> traverse \foreignLib -> do
-  foreignRefs <- newIORef 0
-  pure ForeignSrc {..}
+loadForeignSrc = loadForeignLib >=> traverse \ptr ->
+  -- Set up the destructor to automatically close the library when there are no
+  -- more references to it
+  ForeignSrc <$> newForeignPtr ptr (unloadForeignLib ptr)
 
-loadForeignLib :: FilePath -> IO (Either FFILoadError ForeignLib)
+loadForeignLib :: FilePath -> IO (Either FFILoadError (Ptr ()))
 #ifdef darwin_HOST_OS
 loadForeignLib path =
   (Right <$> open "dylib") `catchIOError` \e1 ->
@@ -74,23 +53,28 @@ loadForeignLib path =
 loadForeignLib path =
   tryLoad (CantLoadFFISrc path) $ open "so"
 #endif
-  where open ext = dlopen (path -<.> ext) [RTLD_NOW]
+  where open ext = undl <$> dlopen (path -<.> ext) [RTLD_NOW]
 
-unloadForeignLib :: ForeignLib -> IO ()
-unloadForeignLib = dlclose
+unloadForeignLib :: Ptr () -> IO ()
+unloadForeignLib = dlclose . DLHandle
 
-data ForeignImpl = forall a. ForeignImpl (ForeignPtr a)
+data ForeignImpl = ForeignImpl
+  { foreignImplFun :: FunPtr ()
+    -- We don't need this to call the function but we want to keep the library
+    -- around as long as we still have a function from it so that the destructor
+    -- isn't called too early
+  , foreignImplLib :: ForeignPtr ()
+  }
 
 loadForeignImpl :: ForeignSrc -> String -> IO (Either FFILoadError ForeignImpl)
-loadForeignImpl ForeignSrc {..} name = tryLoad (CantLoadFFIImpl name) do
-  ptr <- castFunPtrToPtr <$> loadForeignFunPtr foreignLib name
-  atomicModifyIORef' foreignRefs (\n -> (succ n, ()))
-  ForeignImpl <$> newForeignPtr ptr do
-    n <- atomicModifyIORef' foreignRefs (\n -> let n' = pred n in (n', n'))
-    when (n == 0) $ unloadForeignLib foreignLib
+loadForeignImpl (ForeignSrc foreignImplLib) name =
+  withForeignPtr foreignImplLib \lib ->
+    tryLoad (CantLoadFFIImpl name) do
+      foreignImplFun <- loadForeignFunPtr lib name
+      pure ForeignImpl {..}
 
-loadForeignFunPtr :: ForeignLib -> String -> IO (FunPtr a)
-loadForeignFunPtr = dlsym
+loadForeignFunPtr :: Ptr () -> String -> IO (FunPtr ())
+loadForeignFunPtr = dlsym . DLHandle
 
 tryLoad :: (String -> FFILoadError) -> IO a -> IO (Either FFILoadError a)
 tryLoad err = fmap (first $ err . displayException) . tryIOError
@@ -118,8 +102,8 @@ instance FFIType Word64 where
 data SomeFFIType = forall a. FFIType a => SomeFFIType a
 
 callForeignImpl :: forall a. FFIType a => ForeignImpl -> [SomeFFIType] -> IO a
-callForeignImpl (ForeignImpl fp) xs = withForeignPtr fp \p ->
-  callFFI (castPtrToFunPtr p) (ffiRet @a) $ map toArg xs
+callForeignImpl ForeignImpl {..} xs = withForeignPtr foreignImplLib \_ ->
+  callFFI foreignImplFun (ffiRet @a) $ map toArg xs
   where toArg (SomeFFIType x) = ffiArg x
 
 #endif
