3cdb7034137f0925d1931d5160aacad2215d2aae
Henry Blanchette <henry@monkeyface.com>
2022-08-12 23:04:48 -0700

    properly handle censoring warnings

--- src/Cryptol/TypeCheck/Monad.hs
+++ src/Cryptol/TypeCheck/Monad.hs
@@ -245,9 +245,6 @@ data RO = RO
   , iPrimNames :: !PrimMap
 
   , iSolveCounter :: !(IORef Int)
-
-    -- matches the user option `warnNonExhaustiveConstraintGuards`
-  , iWarnNonExhaustiveConstraintGuards :: Bool
   }
 
 -- | Read-write component of the monad.
@@ -949,9 +946,6 @@ withMonoType (x,lt) = withVar x (Forall [] [] (thing lt))
 -- | The sub-computation is performed with the given variables in scope.
 withMonoTypes :: Map Name (Located Type) -> InferM a -> InferM a
 withMonoTypes xs m = foldr withMonoType m (Map.toList xs)
-
-askWarnNonExhaustiveConstraintGuards :: InferM Bool
-askWarnNonExhaustiveConstraintGuards = IM $ asks iWarnNonExhaustiveConstraintGuards
 
 --------------------------------------------------------------------------------
 -- Kind checking
