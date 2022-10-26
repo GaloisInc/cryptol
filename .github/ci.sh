29a63a09ef0e3c7ef44dfeb86783a6c82d78f115
Iavor Diatchki <iavor.diatchki@gmail.com>
2022-10-26 14:05:48 -0700

    Copy the syntax-highlight directory under share/cyrptol for installation

--- .github/ci.sh
+++ .github/ci.sh
@@ -102,6 +102,7 @@ bundle_files() {
   cp docs/*pdf $doc
   mkdir -p $lib
   cp -r lib/* $lib
+  cp -r syntax-highlight $lib
 
   # Copy the two interesting examples over
   cp docs/ProgrammingCryptol/{aes/AES,enigma/Enigma}.cry $doc/examples/
