a85c3a9228a8a39426d610daa6b974d97500202b
Iavor Diatchki <iavor.diatchki@gmail.com>
2022-12-02 15:47:49 -0800

    Add type annotations

--- cryptol-remote-api/python/cryptol/file_deps.py
+++ cryptol-remote-api/python/cryptol/file_deps.py
@@ -1,12 +1,14 @@
-def nestedFileDeps(getDeps, name, isFile):
-  done  = {}
+from typing import Dict, Any
+
+def nestedFileDeps(getDeps : Any, name : str, isFile : bool) -> Any:
+  done : Dict[str,Any]  = {}
   start = getDeps(name, isFile)
   todo  = start["imports"].copy()
   while len(todo) > 0:
     m = todo.pop()
     if m in done:
       continue
-    deps = file_deps(m, False)
+    deps = getDeps(m, False)
     todo.extend(deps["imports"])
   return (start, deps)
 
