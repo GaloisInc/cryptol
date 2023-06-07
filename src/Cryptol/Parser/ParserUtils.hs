b36f3ade46a1f45f93b9a964d12b42aabba6a448
Bretton <qsctr@users.noreply.github.com>
2023-06-07 11:38:19 -0700

    Name main modules using their file path

--- src/Cryptol/Parser/ParserUtils.hs
+++ src/Cryptol/Parser/ParserUtils.hs
@@ -47,7 +47,7 @@ import Cryptol.Parser.Utils (translateExprToNumT,widthIdent)
 import Cryptol.Utils.Ident( packModName,packIdent,modNameChunks
                           , identAnonArg, identAnonIfaceMod
                           , modNameArg, modNameIfaceMod
-                          , modNameToText, modNameIsNormal
+                          , modNameToText, mainModName, modNameIsNormal
                           , modNameToNormalModName
                           , unpackIdent, isUpperIdent
                           )
@@ -73,6 +73,8 @@ parse cfg p cs    = case unP p cfg eofPos S { sPrevTok = Nothing
 newtype ParseM a =
   P { unP :: Config -> Position -> S -> Either ParseError (a,S) }
 
+askConfig :: ParseM Config
+askConfig = P \cfg _ s -> Right (cfg, s)
 
 lexerP :: (Located Token -> ParseM a) -> ParseM a
 lexerP k = P $ \cfg p s ->
@@ -1203,10 +1205,14 @@ mkIfacePropSyn mbDoc d =
 
 -- | Make an unnamed module---gets the name @Main@.
 mkAnonymousModule :: [TopDecl PName] -> ParseM [Module PName]
-mkAnonymousModule = mkTopMods Nothing
-                  . mkModule Located { srcRange = emptyRange
-                                     , thing    = mkModName [T.pack "Main"]
-                                     }
+mkAnonymousModule ds =
+  do src <- cfgSource <$> askConfig
+     mkTopMods Nothing $
+     mkModule Located
+       { srcRange = emptyRange
+       , thing    = mainModName src
+       }
+       ds
 
 -- | Make a module which defines a functor instance.
 mkModuleInstanceAnon :: Located ModName ->
