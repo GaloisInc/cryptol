90f7fcefd32e275077fb8bf5ffd44f3163553343
Bretton <qsctr@users.noreply.github.com>
2023-08-29 17:04:05 -0700

    Do apSubst on Type field of EPropGuards for TVars

--- src/Cryptol/TypeCheck/Subst.hs
+++ src/Cryptol/TypeCheck/Subst.hs
@@ -435,7 +435,9 @@ instance TVars Expr where
 
         EWhere e ds   -> EWhere !$ (go e) !$ (apSubst su ds)
 
-        EPropGuards guards ty -> EPropGuards !$ (\(props, e) -> (apSubst su `fmap'` props, apSubst su e)) `fmap'` guards .$ ty
+        EPropGuards guards ty -> EPropGuards
+          !$ (\(props, e) -> (apSubst su `fmap'` props, apSubst su e)) `fmap'` guards
+          !$ apSubst su ty
 
 instance TVars Match where
   apSubst su (From x len t e) = From x !$ (apSubst su len) !$ (apSubst su t) !$ (apSubst su e)
