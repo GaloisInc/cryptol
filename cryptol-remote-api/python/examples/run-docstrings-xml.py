a8b90be149a9a5bc331b60ee9bef33bc718c689f
Iavor S. Diatchki <diatchki@galois.com>
2025-06-05 17:03:48 -0700

    Clean up the script and add junit-xml example

--- cryptol-remote-api/python/examples/run-docstrings-xml.py
+++ cryptol-remote-api/python/examples/run-docstrings-xml.py
@@ -1,28 +1,35 @@
 import os
 import sys
 from cryptol.connection import connect
+from junit_xml import TestSuite, TestCase
 
+use_ansi_color = False
 
 cry = connect(url="http://localhost:8080/", reset_server=True)
 
 project_file = os.path.abspath(sys.argv[1]
                                 if len(sys.argv) > 1
                                 else "run-docstrings-data/project.toml")
-print("Using " + os.path.relpath(project_file))
 
 project_dir = project_file
 if not os.path.isdir(project_dir):
     project_dir = os.path.dirname(project_dir)
 
-
 # Some colors to make things pretty
-def ok(x):
-    return "\033[1;32m" + x + "\033[0m"
-def bad(x):
-    return "\033[1;31m" + x + "\033[0m"
-def output(x):
-    return "\033[36m" + x + "\033[0m"
+def ansi_color(a, y):
+    if use_ansi_color:
+        return "\033[" + a + "m" + y + "\033[0m"
+    else:
+        return y
 
+def ok_color(x):
+    return ansi_color("1;32",x)
+
+def bad_color(x):
+    return ansi_color("1;31",x)
+    
+def output_color(x):
+    return ansi_color("36",x)
 
 # Load the project, specifically modules containing untested docstrings
 proj = cry.load_project(project_file, "untested").result()
@@ -35,6 +42,8 @@ for obj in proj["test_results"]:
         known[obj["file"]] = obj["result"]
 
 
+test_cases=[]
+
 # Check if we need to test anything
 for obj in proj["scan_status"]:
     if not "file" in obj:
@@ -42,7 +51,7 @@ for obj in proj["scan_status"]:
 
     file = obj["file"]
     test_name = os.path.relpath(file, start=project_dir)
-    print(test_name)
+    test_case = TestCase(test_name, classname="Cryptol", file=file)
 
     # File does not contain errors
     if "scanned" in obj:
@@ -50,45 +59,56 @@ for obj in proj["scan_status"]:
 
         # Do we need to run the tests?
         if not s["changed"] and file in known:
+            
             result = known[file]
-            print(2 * ' ' + "Unchanged:", ok("OK") if result else bad("FAIL"))
+            if not result:
+                test_case.status = "failure"
+                test_case.add_failure_info("(cached)")
+            else:
+                test_case.stdout="(cached)"
+            test_cases.append(test_case)
             continue
 
+        output = []
         # Yes, we need to run the tests
         for m in s["parsed"]:
             mod = m["module"]
-            print(2*' ' + "module", mod)
             cry.focus_module(mod)
             check = cry.check_docstrings(cache_id).result()
             cache_id = check["cache_id"]
 
             # Show results of testing each docstring
             for r in check["results"]:
-                print(4*' ' + "Docstring on", r["name"])
+                decl = r["name"]
+                output.append(decl)
 
                 # Each fance block in the docstrings
                 for fence in r["fences"]:
-
+                    
                     # Each command in the sequence
                     for cmd in fence:
                         cmd_result = cmd["result"]
-
-                        # Show command
-                        print(6 * ' ', (ok if cmd_result["success"] else bad)(cmd["input"]), sep="")
-                        # Show output
+                        ok = cmd_result["success"]
+                        color = ok_color if ok else bad_color
+                        output.append(color("  " + cmd["input"]))
                         for l in cmd["log"].splitlines():
-                            print(8 * ' ',output(l), sep="")
-                        # Show result
-                        val = cmd_result["value"]
-                        if not val is None:
-                            print(6 * ' ', val, sep="")
-                        ty = cmd_result["type"]
-                        if not ty is None:
-                            print(6 * ' ', ty, sep="")
+                            output.append(output_color("    " + l))
+                            
+                        if not ok:
+                            test_case.status = "failure"
+                            test_case.add_failure_info(decl)
+        test_case.stdout = "\n".join(output)
 
     # we faild to load the modules because they are invalid
     # (e.g., have parse errors)
     elif "invalid_module" in obj:
-        print("  Error:", obj["invalid_module"]["error"])
+        test_case.status = "error"
+        test_case.add_error_info(message=obj["invalid_module"]["error"])
     elif "invalid_dep" in obj:
-        print("  Depends on a module with an error")
+        test_case.status = "error"
+        test_case.add_error_info(message="Depends on a module with erorr")
+
+    test_cases.append(test_case)
+
+test_suite=TestSuite(os.path.dirname(os.path.relpath(project_file)), test_cases)
+print(TestSuite.to_xml_string([test_suite]))
