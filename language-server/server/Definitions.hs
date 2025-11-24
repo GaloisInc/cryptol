9db163463a7f0156242c9184e5e448f73707836b
Rawane Issa <rissa@galois.com>
2025-11-24 11:53:14 -0800

    tab-comp: Missing import in language-server

--- language-server/server/Definitions.hs
+++ language-server/server/Definitions.hs
@@ -9,6 +9,7 @@ import Cryptol.Utils.PP
 import Cryptol.Utils.RecordMap
 import Cryptol.Utils.Ident
 import Cryptol.Parser.AST
+import Cryptol.Parser.Name
 import Cryptol.ModuleSystem.Name
 import Cryptol.ModuleSystem.NamingEnv
 import Cryptol.TypeCheck.AST qualified as T
